1fb9f66cf7f7f525f34c0cdf70687f52218b8690
Zhenlei Jia <zhenlei.jia@gmail.com>
2021-06-22 08:16:35 -0400

    greeter uses the properties of inherited bindingContext

--- app/components/greeter/greeter.ts
+++
@@ -1,7 +0,0 @@
-export function onload(args) {
-    console.log('greeter "onload" is called')
-    let container = args.object
-    container.bindingContext = {
-        greeterMessage: `Hello, ${container.user}`
-    }
-}
